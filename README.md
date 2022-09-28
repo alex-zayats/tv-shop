7b18669d07d58165195e0e6998af0b565de959ca
unknown <alex3953898@yandex.ru>
2022-09-28 19:57:23 +0200

    dev: add project files

---
+++ README.md
@@ -0,0 +1,21 @@
+# TV-Shop Angular App
+
+S3 Bucket link - https://s3.eu-west-1.amazonaws.com/tv-app.s3-bucket
+
+Angular version: ~12.
+
+All the necessary API endpoints are in the environments files `environment.ts` (for dev builds). Also it contains feature flags to enable/disable endpoints invocations from within the app so to ensure that you don't get errors for not implemented API endpoints.
+
+## Contribution
+
+Create an issue with the detailed description of the improvement/issue.
+
+## Get up and running
+
+Prerequisites: NodeJS v14
+
+Follow the steps:
+
+- git clone
+- npm i
+- ng serve
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,19 @@
+import { Config } from './config.interface';
+
+export const environment: Config = {
+  production: true,
+  apiEndpoints: {
+    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://k786ffsgib.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://oh3mylp1g3.execute-api.eu-west-1.amazonaws.com/dev',
+    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  },
+  apiEndpointsEnabled: {
+    product: false,
+    order: false,
+    import: false,
+    bff: true,
+    cart: false,
+  },
+};
